cdd4c87d3e7be39292967dfa9866f059d845f622
darora <div@nushackers.org>
2013-03-31 15:06:44 +0800

    add method to get the complete relative (or an absolute path) to a file

--- ass4/project/file_ops.py
+++ ass4/project/file_ops.py
@@ -6,11 +6,19 @@ __author__ = 'darora'
 class FileOps(object):
     @staticmethod
     def get_file_list(dir_path):
+        """
+        dir_path: path to dir
+        returns: *just* the file names
+        """
         try:
             return os.listdir(dir_path)
         except OSError:
             print "Invalid directory path encountered"
             sys.exit(-1)
+
+    @staticmethod
+    def get_full_path(filename, dir_path):
+        return '/'.join([dir_path, filename])
 
     @staticmethod
     def get_file_contents(file_path):
@@ -21,3 +29,5 @@ class FileOps(object):
     @staticmethod
     def get_file_as_tree(file_path):
         return ET.ElementTree(file=file_path)
+
+    
