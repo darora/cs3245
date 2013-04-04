b3c870c3301d8041d22a8ae29395a2c188874fe2
Divyanshu Arora <div@nushackers.org>
2013-04-04 17:38:12 +0800

    add file_ops method to handle the malformed query documents

--- ass4/project/file_ops.py
+++ ass4/project/file_ops.py
@@ -1,5 +1,6 @@
 import os, sys
 import xml.etree.cElementTree as ET
+from itertools import imap
 
 __author__ = 'darora'
 
@@ -28,6 +29,29 @@ class FileOps(object):
 
     @staticmethod
     def get_file_as_tree(file_path):
+        """
+        Returns an etree representing the file.
+        
+        Note::the query files are malformed XML, with multiple top-level nodes. To parse queries, use the ``get_query_as_tree'' method instead.
+        
+        TODO: catch ET.ParseError and return an empty tree, with a logged error
+        """
         return ET.ElementTree(file=file_path)
+
+    @staticmethod
+    def get_query_as_tree(file_path):
+        """
+        Wraps the contents of a query file in a phony 'data' tag, so that etree doesn't die on the multiple top-level nodes.
+        """
+        content = []
+        with open(file_path, 'r') as fl:
+            content.append(fl.readline()) # read the <xml> decl line
+            content.append('<data>')
+            content.extend(fl.readlines())
+            content.append('</data>')
+            text = imap(lambda x: x.strip('\n'), content)
+            text = ' '.join(text)
+            tree = ET.fromstring(text)
+            return tree
 
     
